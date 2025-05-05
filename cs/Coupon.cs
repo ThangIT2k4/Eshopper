1fa1080d03d47899cb1672eadb62be74876818a2
Tran-DuyKhanh <160704421+Tran-DuyKhanh@users.noreply.github.com>
2025-05-05 13:12:52 +0700

    Hien thi hieu ung thanh toan

---
+++ cs/Coupon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebDT.Models.Giam_gia
+{
+    public static class Coupon
+    {
+        private static Dictionary<string, float> couponValues = new Dictionary<string, float>
+    {
+        { "COUPON1", 0.1f },
+        { "COUPON2", 0.2f },
+        { "COUPON3", 0.3f },
+        { "COUPON4", 0.4f }
+    };
+
+        public static float? GetDiscount(string code)
+        {
+            if (couponValues.TryGetValue(code.ToUpper(), out float value))
+            {
+                return value;
+            }
+            return null;
+        }
+    }
+
+
+}
